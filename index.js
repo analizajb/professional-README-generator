225ba7a4a36404fd95c720962257d4d787566c58
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-16 11:48:50 -0700

    generating html

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-console.log('Hello Node!');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 // GIVEN a command-line application that accepts user input
 // WHEN I am prompted for information about my application repository
