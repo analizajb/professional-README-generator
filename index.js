2e3965022ec948a993e2cedcf3bcdf46a81d4253
analizajb <96861913+analizajb@users.noreply.github.com>
2022-05-18 17:27:04 -0700

    updating questions and function to generate readme

--- index.js
+++ index.js
@@ -1,138 +1,168 @@
 const inquirer = require('inquirer');
+const fs = require('fs');
+const generatePage = require('./src/generate-page');
 
-const promptUser = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: 'What is your name? (required)',
-            validate: nameInput => {
-                if (nameInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your name!');
-                    return false;
-                }
+const promptUser = [
+    // Project title
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
             }
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: 'Enter your Github username',
-            validate: githubInput => {
-                if (githubInput) {
-                    return true;
-                } else {
-                    console.log('Please enter your Github username!');
-                    return false;
-                }
-            }
-        },
-    ]);
-};
-
-const prompProject = portfolioData => {
-    portfolioData.projects = [];
-    //if there's no 'projects' array property, create one
-    if (!portfolioData.projects) {
-        portfolioData.projects = [];
-    }
-
-    console.log(`
-    =================
-    Add a New Project
-    ================
-    `);
-     return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'title',
-            message: 'What is the title of your project?',
-            validate: titleInput => {
-                if (titleInput) {
-                    return true;
-                } else {
-                    console.log('Please enter the title of your project!');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            name: 'description',
-            message: 'Provide a description of your project. (required)',
-            validate: descriptionInput => {
-                if (descriptionInput) {
-                    return true;
-                } else {
-                    console.log('Please enter a description for your project!');
-                    return false;
-                }
-            } 
-        },
-        {
-            type: 'input',
-            name: 'install',
-            message: 'What are the requirements for installation?' 
-        },
-        {
-            type: 'input',
-            name: 'usage',
-            message: 'How will your application be used?' 
-        },
-        {
-            type: 'input',
-            name: 'contribution',
-            message: 'Please list the contribution guidelines.' 
-        },
-        {
-            type: 'input',
-            name: 'test',
-            message: 'Please provide the test instructions.' 
-        },
-        {
-            type: 'confirm',
-            name: 'confirmAddProject',
-            message: 'Would you like to enter another project?',
-            default: false
         }
-    ])
-
-    .then(projectData => {
-        portfolioData.projects.push(projectData);
-        if (projectData.confirmAddProject) {
-            return prompProject(portfolioData);
-        } else {
-            return portfolioData;
+    },
+    // Project Description
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of your project. Think "who, what why?" (required',
+        validate: descriptionInput => {
+            if (descriptionInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
         }
+    },
+    // Project Installation
+    {
+        type: 'input',
+        name: 'install',
+        message: 'What are the requirements to install your project? (required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Project Usage Info
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'How will your project be used? (required)',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Project Licensing
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Choose a license for your project or choose none. (required)',
+        choices: ['MIT', 'BSD', 'Ms-PL', 'Apache', 'None'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Project Contributions
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Please list the contribtors involved in your project. (required)',
+        validate: contributionInput => {
+            if (contributionInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Project Testing
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How are users able to test your project? (required)',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // Project Questions/Future Dev
+    {
+        type: 'input',
+        name: 'quesdev',
+        message: 'Please list any questions or future development plans you have for your project or type N/A. (required)',
+        validate: quesdevInput => {
+            if (quesdevInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // User Email - to be added to Questions section
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Please include your email to be reached for any further contact. (required)',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+    // User GitHub - to be added to Questions section
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Please include your GitHub username to be reached for any further contact. (required)',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('This section is required.');
+                return false;
+            }
+        }
+    },
+];
+
+// Function to generate READme
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err)
+            throw err;
+        console.log('Thank you! Your information has been properly submitted and generated into a README.')
     });
 };
 
+// Function to initialize app
+function init() {
+    inquirer.prompt(promptUser)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile("README.md", generatePage(userInput));
+    });
+};
 
-promptUser()
- .then(prompProject)
- .then(portfolioData => {
-     console.log(portfolioData);
- });
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
-
-// const pageHTML = generatePage(name, github)
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw new Error(err);
-
-//   console.log('README complete! Check out index.html to see the output!');
-// });
-
-// GIVEN a command-line application that accepts user input
-// WHEN I am prompted for information about my application repository
-// THEN a high-quality, professional README.md is generated with the title of my project and sections entitled Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
+// Function call
+init();
